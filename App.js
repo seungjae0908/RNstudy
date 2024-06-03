3bcec8fc7d7a3cffa8b34d4645f8bc46b6596b31
seungjae0908 <tmtmdd123@naver.com>
2024-06-03 17:12:08 +0900

    삭제기능까지 구현

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
+  const [todoGoals, setTodoGoals] = useState([]);
+
+  // 추가 버튼을 누르면 할 일 목록을 추가하는 함수
+  const addGoalHandler = (enteredGoalText) => {
+    // useState setter 메서드의 스냅샷 방식
+    // 콜백 함수의 매개값은 해당 상태 변수의 최신 값이 전달됨.
+    setTodoGoals((currentTodoGoals) => [
+      ...currentTodoGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  };
+
+  const deleteGoalhandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalsContainer}>
+        {/* 
+        ScrollView는 전체 화면이 렌더링 될때 안의 항목들을 전부 렌더링합니다
+        이로 인해 성능 상의 저하가 나타날 수 있습니다.
+        (보이지 않는 영역까지 렌더링을 진행하기 때무에 목록이 많다면 로딩이 길어짐.)
+        FlatList는 보이는 영역만 일단 렌더링을 진행하고,
+        */}
+        <FlatList
+          data={todoGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalhandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+
+const GoalItem = ({ text, id, onDeleteItem }) => {
+  return (
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        onPress={() => onDeleteItem(id)}
+        // Pressable 컴포넌트의 조건부 스타일 렌더링
+        // Press 이벤트가 발생할 때마다 style 적용을 위해 전달한 함수를 호출. pressData로 이벤트 관련 정보 전달.
+        // 현재 이벤특 ㅏpressed된 것이 맞다면 styles.pressedItem을 적용하겠다.
+        // (ios에서 press 이벤트 스타일 처리 방식)
+        style={(pressData) => pressData.pressed && styles.pressedItem}
+      >
+        <Text style={styles.goalText}>{text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default GoalItem;
+
+const styles = StyleSheet.create({
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
+    padding: 8,
+  },
+  pressedItem: {
+    opacity: 0.5,
+  },
+});
